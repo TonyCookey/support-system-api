44a37106fdef1dfa348cb0f70249cf07016c98d1
Anthony Iroegbu <tonyiroegbu59@gmail.com>
2025-06-23 20:01:13 +0100

    Feat: worked on Authentication Mutations, schemas and migrations

---
+++ app/graphql/mutations/login.rb
@@ -0,0 +1,21 @@
+module Mutations
+  class Login < BaseMutation
+    argument :email, String, required: true
+    argument :password, String, required: true
+
+    field :user, Types::UserType, null: true
+    field :token, String, null: true
+    field :errors, [String], null: false
+
+    def resolve(email:, password:)
+      user = User.find_by(email: email)
+
+      if user&.authenticate(password)
+        token = JsonWebToken.encode(user_id: user.id)
+        { user: user, token: token, errors: [] }
+      else
+        { user: nil, token: nil, errors: ['Invalid email or password'] }
+      end
+    end
+  end
+end
---
+++ app/graphql/mutations/register.rb
@@ -0,0 +1,23 @@
+module Mutations
+  class Register < BaseMutation
+    argument :name, String, required: true
+    argument :email, String, required: true
+    argument :password, String, required: true
+    argument :role, String, required: true
+
+    field :user, Types::UserType, null: true
+    field :token, String, null: true
+    field :errors, [String], null: false
+
+    def resolve(name:, email:, password:, role:)
+      user = User.new(name: name, email: email, password: password, role: role)
+
+      if user.save
+        token = JsonWebToken.encode(user_id: user.id)
+        { user: user, token: token, errors: [] }
+      else
+        { user: nil, token: nil, errors: user.errors.full_messages }
+      end
+    end
+  end
+end
